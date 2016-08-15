946ffd0e45757e5b2d58705813aebefe2e3d4f01
prashant.gaikwad <prashant.gaikwad@accenture.com>
2016-08-15 16:44:16 -0500

    my 1st change

--- mynewapi/GetGreetings/handler.js
+++ mynewapi/GetGreetings/handler.js
@@ -2,6 +2,6 @@
 
 module.exports.handler = function(event, context, cb) {
   return cb(null, {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
+    message: 'Go Serverless changes! Your Lambda function executed successfully!'
   });
 };
