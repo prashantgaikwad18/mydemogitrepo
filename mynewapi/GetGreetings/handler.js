c58485d05de1b21fab22f6a2aa5f6dec39974348
prashant.gaikwad <prashant.gaikwad@accenture.com>
2016-08-16 11:06:44 -0500

    added meta folder

--- mynewapi/GetGreetings/handler.js
+++ mynewapi/GetGreetings/handler.js
@@ -2,6 +2,6 @@
 
 module.exports.handler = function(event, context, cb) {
   return cb(null, {
-    message: 'Go Serverless MORE More changes! Your Lambda function executed successfully!'
+    message: 'Go Serverless LOT OF changes! Your Lambda function executed successfully!'
   });
 };
